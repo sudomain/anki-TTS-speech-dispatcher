16fef1e69debe4645b35df77b2c3ec3e1c97a375
sudomain <sudomain@github>
2023-03-21 12:42:30 -0400

    update TODO list

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Anki TTS Speech Dispatcher
-Adding speech-dispatcher support to Anki through an add-on.
+Adding speech-dispatcher support to Anki through an add-on. It uses `spd-say` to speak text, so if that works then this add-om should work.
 
 This add-on is new and hasn't been extensively test. It assumes you have a properly configured speech-dispatcher and that `spd-say` works (this add-on uses the defailt voice). Some limitations which I hope to fix:
-1. "en_US" is hardcoded into the add-on
+1. "en_US" is hardcoded into the add-on though isn't used. If speechd is configured to use another language, it should work, but specifying different languages in the field should have no effect e.g. `{{tts fr_FR:SomeField}}`
 2. Field TTS speed control is not yet supported e.g. `{{tts fr_FR speed=0.8:SomeField}}`
+3. config file - add variable so reading can be disabled while reviewing
 
 Speech Dispatcher is configured using separate config files. It allows for many customizations including different speech settings for different applications (Firefox, Emacs, and now Anki). It works with many TTS engines. If an engine is not already supported, the process of making a new one is relatively simple using the "generic output module". See the Configuration section of the speech-dispatcher Texinfo document for file locations and examples.
 
--- __init__.py
+++ __init__.py
@@ -6,37 +6,36 @@ Speech Dispatcher is configured using separate config files. It allows for many 
 Heavily based on MacTTSPlayer(TTSProcessPlayer): https://github.com/ankitects/anki/blob/f9126927b1c537f76b2e11f2f8a6003d2313245b/qt/aqt/tts.py#L170
 """
 
-#TODO list voices with spd-say -L and parsing the output
-#TODO remove hardcoded language "en_US" 
-#TODO test stopping with an output module known to support it such as espeak.
-#TODO support per field TTS speed e.g. {{tts fr_FR speed=0.8:SomeField}}
+# `info spd-say` should have most of the information needed to complete this TODO list, I just need more time
+#TODO spd-say --pipe-mode might give better performance since it will keep an instance of spd-say reading from stdin and we can write to that using subprocess. There's an example in old-attempts/ directory
+#TODO list voices with spd-say -L and parsing the output tab separated value of "NAME LANGUAGE VARIANT" however we need to account for NAME containing spaces such as "Chinese (Mandarin)"
+#TODO remove hardcoded language "en_US" using spd-say --language 
+#TODO test stopping with an output module known to support it such as espeak
+#TODO remove unnecesary imports
+#TODO support per field TTS speed e.g. {{tts fr_FR speed=0.8:SomeField}}. spd-say --rate can take values between -100 and 100 so the decimal values Anki uses must be normalizaed to integers between -100 and 100 
 
-import os
-import sys
-from concurrent.futures import Future
+import subprocess
 from dataclasses import dataclass
 from typing import List, cast
 
-from anki.lang import compatMap
 from anki.sound import AVTag, TTSTag
 from aqt import mw
 from aqt.sound import OnDoneCallback, av_player
-#from aqt.tts import TTSProcessPlayer, TTSVoice
 
-import subprocess
 import aqt.tts
 
 # we subclass the default voice object to store the speechd language code
 @dataclass
 class SpeechDVoice(aqt.tts.TTSVoice):
+    #TODO Getting the voice list from `spd-say --list-synthesis-voices` can take several seconds if many are installed, so do it at Anki startup
     speechd_lang: str
 
 #class SpeechDPlayer(TTSProcessPlayer):
 class SpeechDPlayer(aqt.tts.TTSProcessPlayer):
     # this is called the first time Anki tries to play a TTS file
-    #TODO spd -L gives a list of voices, but I dont think it's in a format that can be used
     def get_available_voices(self) -> List[aqt.tts.TTSVoice]:
         voices = []
+
         # add the voice using the name "speechd"
         voices.append(SpeechDVoice(name="speechd", lang="en_US", speechd_lang="en"))
         return voices  # type: ignore
@@ -52,20 +51,6 @@ class SpeechDPlayer(aqt.tts.TTSProcessPlayer):
             return
 
         subprocess.run(["spd-say", "--wait", "--application-name", "Anki", tag.field_text])
-        #TODO pipe mode can be sent commands if the line starts with !-!, for instance !-!SET SELF VOICE FEMALE1. Use Python to prepend a space to deactivate commands
-        """
-        self._process = subprocess.Popen(
-            ["spd-say", "--application-name", "Anki", "--pipe-mode"],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-        
-        # write the input text to stdin
-        self._process.stdin.write(tag.field_text.encode("utf8"))
-        self._process.stdin.close()
-        self._wait_for_termination(tag)
-        """
         return
 
     # this is called on the main thread, after _play finishes
